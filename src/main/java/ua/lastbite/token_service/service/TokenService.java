e61eb5281f8c6ca07918849581e10bd70fddeb76
Augustin Gerasym <augustingerasym@gmail.com>
2025-01-26 18:41:57 +0100

    Removed redudant import statements.

--- src/main/java/ua/lastbite/token_service/service/TokenService.java
+++ src/main/java/ua/lastbite/token_service/service/TokenService.java
@@ -1,8 +1,6 @@
 package ua.lastbite.token_service.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
